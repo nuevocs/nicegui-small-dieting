d54ba32ac97fd625eadfe5e1fac1a9e84f89a2e6
Tatsuya Tomita <tat@seriousexplosion.net>
2023-05-07 21:48:23 +0900

    supabase

---
+++ nicegui_small_dieting/db/supabase_utilities.py
@@ -0,0 +1,36 @@
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+from dataclasses import asdict, dataclass
+
+load_dotenv()
+
+
+def supabase_client() -> Client:
+    # client initialization
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_SECRET")
+    supabase: Client = create_client(url, key)
+    return supabase
+
+
+def as_dict_supabase(table_name: str, data) -> dict:
+    supabase = supabase_client()
+    dct = asdict(data)
+    send_data = supabase.table(table_name).insert(dct).execute()
+    assert len(send_data.data) > 0
+    return asdict(data)
+
+
+def select_all(table_name: str, column_name: str):
+    supabase = supabase_client()
+    data = supabase.table(table_name).select(column_name).execute()
+    assert len(data.data) > 0
+    return data.data
+
+
+def select_filtered(table_name: str, cols: str, filter_col: str, filter_value: str):
+    supabase = supabase_client()
+    data = supabase.table(table_name).select(cols).eq(filter_col, filter_value).execute()
+    assert len(data.data) > 0
+    return data.data
--- nicegui_small_dieting/main.py
+++ nicegui_small_dieting/main.py
@@ -1,5 +1,6 @@
 from nicegui import ui
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from db.supabase_utilities import supabase_client, select_all, as_dict_supabase
 
 NUTRITION_CATEGORY = {
     1: 'Dairy',
@@ -16,10 +17,10 @@ SITE_TITLE = "Small Dieting Projects"
 
 
 def page_layout_consumbles():
-    with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
+    with ui.header(elevated=True).style('background-color: #333333').classes('items-center justify-between'):
         ui.label(SITE_TITLE).classes("text-xl text-center")
         ui.button(on_click=lambda: right_drawer.toggle()).props('flat color=white icon=menu')
-    with ui.footer().style('background-color: #3874c8'):
+    with ui.footer().style('background-color: #666666'):
         ui.label('copyright nuevocs.dev').classes("text-sm text-center")
     with ui.right_drawer(fixed=False).style('background-color: #fcfcfc; width:"350"').props(
             'bordered mini-to-overlay width="250" show-if-above="False"') as right_drawer:
@@ -50,7 +51,7 @@ class Nutrition:
             self.nutrition_carbohydrate = ui.slider(min=0, max=300, value=0)
             self.nutrition_carbohydrate_label = ui.label().bind_text_from(self.nutrition_carbohydrate, 'value')
         with ui.column().classes('w-full'):
-            self.nutrition_amount = ui.number(label="quantity",  on_change=lambda: self.current_calories())
+            self.nutrition_amount = ui.number(label="quantity", on_change=lambda: self.current_calories())
 
     def current_calories(self):
         global PROTEIN, FAT, CARBOHYDRATE
@@ -68,35 +69,55 @@ class PhysicalTraining:
 
 @dataclass
 class NutritionData:
-    name: str
-    category: str
-    protein: int
-    fat: int
-    carbohydrate: int
-    amount: int
-    calories: float
+    nutrition_name: str
+    nutrition_category: str
+    nutrition_protein: int
+    nutrition_fat: int
+    nutrition_carbohydrate: int
+    nutrition_amount: int
+    nutrition_calories: float
 
+
+# column_name and dataclass attribute name must be the same
 
 @ui.page('/physical-training')
 async def ptpage_content():
     page_layout_consumbles()
+
 
 @ui.page('/')
 async def main_content():
     def get_data_from_inputs():
         global PROTEIN, FAT, CARBOHYDRATE
         nutrition_data = NutritionData(
-            name=nutrition.nutrition_name.value,
-            category=nutrition.nutrition_category.value,
-            protein=nutrition.nutrition_protein.value,
-            fat=nutrition.nutrition_fat.value,
-            carbohydrate=nutrition.nutrition_carbohydrate.value,
-            amount=nutrition.nutrition_amount.value,
-            calories=nutrition.nutrition_protein.value * PROTEIN * nutrition.nutrition_amount.value + nutrition.nutrition_fat.value * FAT * nutrition.nutrition_amount.value + nutrition.nutrition_carbohydrate.value * CARBOHYDRATE * nutrition.nutrition_amount.value
+            nutrition_name=nutrition.nutrition_name.value,
+            nutrition_category=nutrition.nutrition_category.value,
+            nutrition_protein=nutrition.nutrition_protein.value,
+            nutrition_fat=nutrition.nutrition_fat.value,
+            nutrition_carbohydrate=nutrition.nutrition_carbohydrate.value,
+            nutrition_amount=nutrition.nutrition_amount.value,
+            nutrition_calories=nutrition.nutrition_protein.value * PROTEIN * nutrition.nutrition_amount.value + nutrition.nutrition_fat.value * FAT * nutrition.nutrition_amount.value + nutrition.nutrition_carbohydrate.value * CARBOHYDRATE * nutrition.nutrition_amount.value
         )
         collection.append(nutrition_data)
         collection_table.update()
-        print(nutrition_data)
+
+        # reset all inputs
+        nutrition.nutrition_name.value = ""
+        nutrition.nutrition_category.value = 0
+        nutrition.nutrition_protein.value = 0
+        nutrition.nutrition_fat.value = 0
+        nutrition.nutrition_carbohydrate.value = 0
+        nutrition.nutrition_amount.value = 0
+
+        # print(nutrition_data)
+
+    def writing_data_to_db(collection):
+        nbr_records = len(collection)
+        ui.notify(f"{nbr_records} records has been written to the database")
+        for c in collection:
+            as_dict_supabase("nutrition_details", c)
+        collection.clear()
+        collection_table.update()
 
     page_layout_consumbles()
 
@@ -114,7 +135,6 @@ async def main_content():
                     nutrition = Nutrition()
                     ui.button("add", on_click=lambda: get_data_from_inputs())
 
-
         with ui.card():
             with ui.card_section():
                 with ui.column():
@@ -123,14 +143,14 @@ async def main_content():
                     """).classes("text-sm text-gray-600/75")
                     collection = []
                     header = [
-                        {"name": "name", "label": "Name", "field": "name"},
-                        {"name": "amount", "label": "Amount", "field": "amount"},
-                        {"name": "calories", "label": "Calories", "field": "calories", "required": True,
+                        {"name": "nutrition_name", "label": "Name", "field": "nutrition_name"},
+                        {"name": "nutrition_amount", "label": "Amount", "field": "nutrition_amount"},
+                        {"name": "calories", "label": "Calories", "field": "nutrition_calories", "required": True,
                          "sortable": True,
                          "align": "left"},
                     ]
                     collection_table = ui.table(columns=header, rows=collection)
-                    ui.button("submit", on_click=lambda: print(collection))
+                    ui.button("submit", on_click=lambda: writing_data_to_db(collection))
 
 
 ui.run()
